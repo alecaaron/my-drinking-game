1ac53c3206eb3ca0271a51a234bc79e14c46279c
Alec Aaron <alecalegria@gmail.com>
2016-08-09 23:38:00 -0500

    added reset function when deck runs out

--- js/app.js
+++ js/app.js
@@ -1,43 +1,26 @@
 // Initialize game by serving a random cards
-pressStart();
 
-// Capsulize MVC-controler to program for 
-// controls
 function pressStart(){
-    gameObj.shuffle();
-    var $img = document.getElementById('image');
-    var $name = document.getElementById('name');
-    var $message = document.getElementById('desc');
-    
-    //Bind the data to the DOM
-    $img.innerHTML = '<img src=' + gameObj.current_rule.img + ' alt=\'gameImg\' >';
-    $name.innerHTML = gameObj.current_rule.title;
-    $message.innerHTML = gameObj.current_rule.desc;
-    
-    //Process your data; Place the current rule into
-    //the graveyard & delete it from play
-    gameObj.pross(gameObj.current_rule, randNum);
-}
+  gameObj.shuffle();
+  gameObj.print_rule();
+};
 
-
-//This function encapsulates a function
-//for easy removal for event binding
-function keyfunc(){
+/*This function encapsulates a function
+for easy removal of event binding when
+deck is finished*/
+function keyfunc(e){
     if(e.keyCode == 13 || e.keyCode == 32){
         pressStart();
     }
 }
-
-
-
+pressStart();
 
 //****************** EVENTS / CONTROLS ************
 
 
-// Capture the shuffle button and 
+// Capture the shuffle button and
 // serve a a game when clicked.
 var nxtBtn = document.getElementById('shuffle_button');
 nxtBtn.addEventListener('click', pressStart);
 //Add keyboard support.
 window.addEventListener('keypress', keyfunc);
-
--- js/gameObj.js
+++ js/gameObj.js
@@ -3,67 +3,107 @@
 var gameObj = {
 
 
-//******** Begin Properties *******************************    
+//******** Begin Properties *******************************
     rules: [
         {
             title: "Double Shot",
             desc: "Roll some dice, Highest number takes a shot",
             img: "imgs/obj_A.jpg"
         },
-        
+
         {
             title: "Straight Shooter",
             desc: "Take a shot straight, as in no chaser",
             img: "imgs/obj_B.jpg"
         },
-        
+
         {
             title: "Santa is Coming!",
-            desc: "Roll a dice, <br/>" + 
+            desc: "Roll a dice, <br/>" +
                     "<span class=\"odd\">Odd</span>: Give two shots</br>" +
-                    "<span class=\"even\">Even<span>: Give 1 shot",
+                    "<span class=\"even\">Even</span>: Give 1 shot",
             img: "imgs/obj_C.jpg"
         },
-        
+
         {
-            title: "",
-            desc: "",
-            img: ""
+            title: "Top Up",
+            desc: "Take time to fill up your cups.",
+            img: "imgs/obj_d.jpg"
         }
     ],
-    
+
+    //Discarded cards
     graveyard: [],
-        
-    
+
+    //Ending & beginning of deck
+    edgeOfDeck: [
+      {
+        title: "ready for this round?",
+        desc: "Press the button, or use Shift & Enter on your keyboard",
+        img: "deckBegin"
+      },
+
+      {
+        title: "Sorry!",
+        desc: "I guess you've hit the end of the game. Refresh your window for the next round.",
+        img: "deckEnd"
+      }
+    ],
+
+    current_rule: "",
+
 //*********** BEGIN METHODS **********************
 
-    //This function will pick out a random card and push it into the DOM
-    shuffle: function () {
-        
-        window.randNum = Math.floor(Math.random() * gameObj.rules.length);
-        
-        var rule = gameObj.rules[randNum];
-        
-        this.current_rule = rule;
-        
-        if(gameObj.rules.length == 0){
-            
-            gameObj.current_rule = {
-                title: "Sorry!",
-                desc: "I guess you've hit the end of tha game. Refresh your window for Round 2.",
-                img: "xyz"
-            };
-            
-            window.removeEventListener('keypress', keyfunc);
-            nxtBtn.removeEventListener('click', pressStart);
-            
-        }
-    },
-    
-    pross: function(obj, index){
-        gameObj.graveyard.push(obj);
-        gameObj.rules.splice(index, 1);
+    print_rule: function(){
+      var $img = document.getElementById('image')
+      var $name = document.getElementById('name');
+      var $message = document.getElementById('desc');
+
+      //Bind the data to the DOM
+      $img.innerHTML = '<img src=' + this.current_rule.img + ' alt=\'gameImg\' >';
+      $name.innerHTML = this.current_rule.title;
+      $message.innerHTML = this.current_rule.desc;
+
     },
 
-    
+
+    /* grab the current rule and push it into the DOM */
+    shuffle: function(){
+        window.randNum = Math.floor(Math.random() * gameObj.rules.length);
+        var rule = gameObj.rules[randNum];
+        gameObj.current_rule = rule;
+
+        if(gameObj.rules.length == 0){
+          gameObj.current_rule = this.edgeOfDeck[1];
+          nxtBtn.setAttribute("value", "Reset");
+        }
+        else if (gameObj.rules.length != 0) {
+          document.getElementById("shuffle_button").setAttribute("value", "Shuffle");
+        }
+
+
+        gameObj.pross(gameObj.current_rule, randNum);
+
+
+
+    },
+
+    //Process used cards to move to graveyard
+    pross: function(obj,index){
+      this.graveyard.push(obj);
+      this.rules.splice(index, 1);
+
+      if(obj == gameObj.edgeOfDeck[1]){
+        gameObj.graveyard.splice((gameObj.graveyard.length - 1), 1);
+        for(var i = 0; i < gameObj.graveyard.length; i++){
+          gameObj.rules.push(gameObj.graveyard[i]);
+        }
+        gameObj.graveyard = [];
+      }
+    }
+
+
+
+
+
 }
